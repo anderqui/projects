02cfc4dbd139441a58389b7968fa21e2d1617da5
unknown <anderqui001@warren.k12.in.us>
2019-04-01 13:08:52 -0400

    commit message alpha

--- game/dot.js
+++ game/dot.js
@@ -1,20 +1,12 @@
 $(document).ready(function(){
- var BB= false
- var TB= false
- var RB= false
- var LB= false
+ var BB= false;
+ var TB= false;
+ var RB= false;
+ var LB= false;
+ var play=true;
 })
     $('td').click(function(){
-        if($(td).text()===""){
-            
-        }
-    });
-    $('button').click(function () {
-        turn = 1;
-        play=true;
-        reset();
-    })
-    for (var i=0;i<47;i++){
-        space.click($("#tile"+i)())
-
-    }
+        ($(td).click()===""&& play=== true){
+            $("tile"++).click(function(){
+                $("td:TB").addClass("topborder");
+  
